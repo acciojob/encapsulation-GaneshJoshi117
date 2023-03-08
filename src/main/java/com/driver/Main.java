273f6796e9bb566243f17abfedd3ef71336dd6b5
GaneshJoshi117 <ganeshjoshi583@gmail.com>
2023-03-08 14:59:25 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        System.out.println(obj.x);
+//        java: x has private access in com.driver.RWOnly
+        obj.setX(5);
+        System.out.println(obj.getX());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    private int x = 1;
 }
